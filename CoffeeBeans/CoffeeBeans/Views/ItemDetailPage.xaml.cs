b5110921d7c721cd3c3a99247201b6fb71c6c1d7
Martin Jensen <martinfhjensen@gmail.com>
2020-09-13 19:59:21 +0200

    Add project files.

---
+++ CoffeeBeans/CoffeeBeans/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using CoffeeBeans.ViewModels;
+
+namespace CoffeeBeans.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
